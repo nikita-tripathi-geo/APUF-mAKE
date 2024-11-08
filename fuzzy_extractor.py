6d129f265c9e50a022e718d5583d0ac676275f88
Nikita Tripathi <nikita.tripathi@ucalgary.ca>
2024-11-07 17:47:47 -0700

    Finished implementation. Basic testing and timing added.

--- fuzzy_extractor.py
+++ fuzzy_extractor.py
@@ -7,8 +7,9 @@ import hmac
 from hashlib import sha3_384, sha384
 import time
 
-# from apuf_simulation import generate_n_APUFs, get_noisy_responses
 from utilities import xor_bytes
+from apuf_simulation import generate_n_APUFs, get_noisy_responses
+import numpy as np
 
 # Developed for Python 3.12.6
 
@@ -31,9 +32,9 @@ class FuzzyExtractor:
         # System parameters
         self.m = sample_len
         self.ell = locker_num
-        self.xi = key_len
-        self.lbd = mac_key_len
-        self.t = padding_len
+        self.xi = key_len // 8
+        self.lbd = mac_key_len // 8
+        self.t = padding_len // 8
         self.nonce_len = nonce_len
 
         # Total message length (0^t || R || R_1)
@@ -43,12 +44,13 @@ class FuzzyExtractor:
         # self.hash = sha384
 
         # Generating a CRS: \ell nonces of length `nonce_len`
-        self.h = [randbits(self.nonce_len) for _ in range(self.ell)]
+        self.h = [token_bytes(self.nonce_len) for _ in range(self.ell)]
 
         # Pre-define ciphertext list and tag
         self.ctxt = [bytes(0) for _ in range(self.ell)]
         self.tag = bytes(0)
 
+        # Pre-compute zero string for faster comparison
         self.zeros = bytes([0]*self.t)
 
 
@@ -57,6 +59,7 @@ class FuzzyExtractor:
             w: list[bytes]
     ) -> bytes:
         '''
+        TODO
         '''
 
         # Generate keys from OS randomness
@@ -72,9 +75,10 @@ class FuzzyExtractor:
             self.ctxt[i] = xor_bytes(msg, pad)
 
         # Calculate tag
-        self.tag = hmac.digest(key=R_1, msg=sum(self.ctxt), digest=sha3_384)
+        self.tag = hmac.digest(key=R_1, msg=b''.join(self.ctxt), digest=sha3_384)
 
         return R
+
 
     def reproduce(
             self,
@@ -93,9 +97,8 @@ class FuzzyExtractor:
                 # Leading t bits are zeros
                 R = msg_[self.t:(self.t + self.xi)]
                 R_1 = msg_[(self.t + self.xi):]
-                print(f"Lengths: R: {len(R)}, R_1: {len(R_1)}")
 
-                tag = hmac.digest(key=R_1, msg=sum(self.ctxt), digest=sha3_384)
+                tag = hmac.digest(key=R_1, msg=b''.join(self.ctxt), digest=sha3_384)
 
                 if tag == self.tag:
                     return R
@@ -103,20 +106,48 @@ class FuzzyExtractor:
         return None
 
 
-# def main(num_rep, LOCKER_NUM):
+def main(num_rep, LOCKER_NUM):
 
-#     # Create seeds for challenges TODO this is hard-coded, find a better way to do this
-#     # challenge_seeds = [random.SystemRandom().randint(0, 2**32 -1) for _ in range(3_500)]
-#     APUF = generate_n_APUFs(1, 129, weight_mean=0, weight_stdev=0.05)
-#     APUF_adversary = generate_n_APUFs(1, 129, weight_mean=0, weight_stdev=0.05)
-#     challenges = np.load("../3500_challenges.npy")[:LOCKER_NUM]
+    APUF = generate_n_APUFs(1, 129, weight_mean=0, weight_stdev=0.05)
+    APUF_adversary = generate_n_APUFs(1, 129, weight_mean=0, weight_stdev=0.05)
+    challenges = np.load("./3500_challenges.npy")[:LOCKER_NUM]
 
-#     # challenges = generate_k_n_challenges(LOCKER_NUM, 192, 129, seed=random.SystemRandom().randint(0, 2**32 -1))
-#     #np.save("3500_challenges.npy", challenges)
 
-#     # resp1 = get_noisy_responses(APUF, challenges, noise_mean=0, noise_std=(0.1*0.05))
-#     # c1 = [resp1[j][0] for j in range(len(resp1))]
-#     c1 = np.concatenate([get_noisy_responses(1, [APUF], chal, noise_mean=0, noise_std=(0.05*0.1)) for chal in challenges  ])
+    fe = FuzzyExtractor()
+
+    # Server obtains sample W
+    server_sample = [
+        bytes(get_noisy_responses(1, [APUF], c, 0, 0.05*0.1 )) for c in challenges
+    ]
+
+    # Server runs Gen(W) to obtain a session key and helper data
+    t = time.perf_counter()
+    key = fe.generate(server_sample)
+    t1 = time.perf_counter()
+
+    print(f"Gen took {t1-t} seconds")
+    print(f"Key = {key}")
+
+
+    for usr_attempt in range(num_rep):
+        
+        # User obtains a fresh sample W' 
+        user_sample = [
+            bytes(get_noisy_responses(1, [APUF], c, 0, 0.05*0.1 )) for c in challenges
+        ]
+
+        t = time.perf_counter()
+        key_ = fe.reproduce(user_sample)
+        t1 = time.perf_counter()
+
+        print(f"Rep took {t1-t} seconds")
+        print(f"Key = {key_}")
+
+        print(f"Attempt id: {usr_attempt}", "Session keys match!" if key == key_ else "Session key mismatch")
+
+
+if __name__ == "__main__":
+    main(10, 3500)
 
 #     print(c1.shape)
 
