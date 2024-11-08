42229346a20939bebcea84b64f6e62451eb28919
Nikita Tripathi <nikita.tripathi@ucalgary.ca>
2024-11-07 23:16:08 -0700

    Added some documentation and unit tests

--- utilities.py
+++ utilities.py
@@ -2,13 +2,47 @@ from sys import byteorder
 
 def xor_bytes(a: bytes, b: bytes) -> bytes:
     '''
+    Performs a byte-wise XOR operation on two byte sequences of equal length.
+    
+    Args:
+        a (bytes): The first byte sequence.
+        b (bytes): The second byte sequence, of the same length as `a`.
+        
+    Returns:
+        bytes: Result of XORing each byte in `a` with the corresponding byte in `b`.
+
+    Examples:
+        >>> xor_bytes(b'\\x0f\\x0f', b'\\x00\\x0f')
+        b'\\x0f\\x00'
+
+        >>> xor_bytes(b'\\x0f\\x0f', b'\\x0f\\x0f')
+        b'\\x00\\x00'
+        
+        >>> xor_bytes(b'\\xaa', b'\\x55')
+        b'\\xff'
+        
+        >>> xor_bytes(b'\\x01\\x02\\x03', b'\\x01\\x01\\x01')
+        b'\\x00\\x03\\x02'
+
+        >>> xor_bytes('string', b'\\x00\\x0f')  # Test non-byte input
+        Traceback (most recent call last):
+            ...
+        TypeError: Both arguments must be of type 'bytes'.
+
     '''
-    # TODO add error checking like different lengths, etc.
-    # TODO add unit tests
+
+    # Type check for bytes
+    if not isinstance(a, bytes) or not isinstance(b, bytes):
+        raise TypeError("Both arguments must be of type 'bytes'.")
 
     int_a = int.from_bytes(a, byteorder)
     int_b = int.from_bytes(b, byteorder)
 
     int_xor = int_a ^ int_b
 
-    return int_xor.to_bytes(len(a), byteorder)
+    return int_xor.to_bytes(max(len(a), len(b)), byteorder)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
